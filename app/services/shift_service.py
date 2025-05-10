f744d5a5dc27b37eb398e5183f2459500b7390c2
EmmaHKarlen <emmahkarlen@gmail.com>
2025-05-10 15:32:58 +0300

    Implement Google Sheets integration in ShiftService; add methods to parse sheets and extract employee preferences

---
+++ app/services/shift_service.py
@@ -0,0 +1,74 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+import sys
+
+sys.path.append('c:/Users/User/Desktop/smartShifts/SmartShifts/app')
+
+from models.time_models import ShiftDate
+from models.shift_preferences import EmployeePreferences, Preference
+
+
+# Define the scope for accessing Google Sheets
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+# Authorize the client using the credentials JSON file
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    'c:/Users/User/Desktop/smartShifts/SmartShifts/app/credentials.json', 
+    scope
+)
+
+class ShiftService:
+    def __init__(self):
+        self.sheet_name = None
+        
+
+    def parse_google_sheet(self, google_link: str, month: int):
+        client = gspread.authorize(credentials)
+        sheet_id = google_link.split('/d/')[1].split('/')[0]
+        sheet = client.open_by_key(sheet_id)
+        worksheets = sheet.worksheets()
+        for worksheet in worksheets:
+            if worksheet.title == "general":
+                continue
+            worksheet_month, _ = self._parse_sheet_name_to_ints(worksheet.title)
+            if worksheet_month == month:
+                self.sheet_name = worksheet.title
+                self.worksheet = worksheet
+                break
+
+        data = self.worksheet.get_all_values()
+        employee_preferences = EmployeePreferences(employee_id = data[0][0], preferences= self._get_preferences_from_data_table(data))
+        return employee_preferences
+
+    def _parse_sheet_name_to_ints(self, sheet_name: str):
+        parts = sheet_name.split(" ")
+        if len(parts) == 2:
+            month_str, year_str = parts[0], parts[1]
+            month = datetime.strptime(month_str, "%B").month
+            year = int(year_str)
+            return month, year
+        else:
+            raise ValueError("Sheet name format is invalid. Expected format: 'Month Year'.")
+
+    def _get_preferences_from_data_table(self, data_table: list):
+        preferences_list = []
+        self.month, self.year = self._parse_sheet_name_to_ints(self.sheet_name)
+        for i in range(1, len(data_table)):
+            for j in range(1, len(data_table[i])):
+                if data_table[i][0] == 'Date':
+                    week_date_list = data_table[i]
+                    break
+                if data_table[i][0] == '' or week_date_list[j] == '':
+                    break
+                shift_date = ShiftDate(day=int(week_date_list[j]), month=self.month, year=self.year)
+                shift_type = data_table[i][0]
+                shift_preference = bool(int(data_table[i][j]))
+
+                preferences_list.append(Preference(start_date=shift_date, shift_type=shift_type, availability=shift_preference))
+        return preferences_list            
+    
+if __name__ == "__main__":
+    emmaz = ShiftService()
+    tomerz = emmaz.parse_google_sheet("https://docs.google.com/spreadsheets/d/1iCGT15DFkr7W93_pfUWME4yUO_byj18N1hZ9zmnGAMA/edit?gid=0#gid=0", 6)
+    print(tomerz)
