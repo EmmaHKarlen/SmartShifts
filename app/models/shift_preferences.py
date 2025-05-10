bb56384a16700021f0cd8c946c6c068d2b94a415
EmmaHKarlen <emmahkarlen@gmail.com>
2025-05-10 15:33:34 +0300

    Refactor shift preferences model; update ShiftType enum values and change availability type to boolean

--- app/models/shift_preferences.py
+++ app/models/shift_preferences.py
@@ -2,29 +2,24 @@ from enum import Enum
 from typing import List
 from pydantic import BaseModel
 
-from app.models.time_models import ShiftDate
+from models.time_models import ShiftDate
 
 class ShiftType(Enum):
-    day = "day"
-    night = "night"
-    standby = "standby"
-
-
-class ShiftAvailability(Enum):
-    unavailable = 0
-    available = 1
-    prefer = 2
+    day = "Day"
+    night = "Night"
+    standby = "Standby"
 
 
 class Preference(BaseModel):
     start_date: ShiftDate
     shift_type: ShiftType
-    availability: ShiftAvailability
+    availability: bool
 
 
 class EmployeePreferences(BaseModel):
-    employee_id = str
-    preferences = List[Preference]
+    employee_id : str
+    preferences : List[Preference]
+
 
 
 
